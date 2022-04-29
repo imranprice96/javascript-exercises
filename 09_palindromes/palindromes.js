d523821d7c0fa7bfcf97fc6dfbbce3857084a1a6
Imran Price <imranprice96@gmail.com>
2022-04-29 23:22:05 +1200

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // lowercase, remove punctuation and spaces, split
+    let arr = str.toString().replace(/[^A-Za-z0-9_]/g, "").toLowerCase().split('');
+    let joined = arr.join('');
+    let reverseJoined = arr.reverse().join('');
+    return joined == reverseJoined;
 
 };
 
