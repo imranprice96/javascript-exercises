34e35b8e886443fc27d01825c1a7f42bab086b68
Imran Price <imranprice96@gmail.com>
2022-04-30 00:13:51 +1200

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((a,b) => {
+        const oldestAge = getAge(a.yearOfBirth, a.yearOfDeath);
+        const nextAge = getAge(b.yearOfBirth, b.yearOfDeath);
+        console.log(oldestAge, nextAge);
+        return oldestAge < nextAge ? b : a;
+    });
 };
+
+const getAge = function (birth, death){
+    if(!death){
+    death = new Date().getFullYear();
+    }
+    return death - birth;
+};
+
+  
+  module.exports = findTheOldest;
 
 // Do not edit below this line
 module.exports = findTheOldest;
