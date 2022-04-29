ae1fe907acede19298ab1d1ee4acc2e22226d194
Imran Price <imranprice96@gmail.com>
2022-04-29 23:41:03 +1200

    Complete exercise 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fibArrray = [];
+    let a = 1, b = 0, temp;
+    let index = num;
+    if(num < 0 )return "OOPS";
+    while(num >=0){
+        temp = a;
+        a = a + b;
+        b = temp;
+        num--;
+        fibArrray.push(b);
+    };
+    return parseInt(fibArrray[index-1]);
 };
 
 // Do not edit below this line
