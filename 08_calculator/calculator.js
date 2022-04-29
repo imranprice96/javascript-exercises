43e19625d48ce399853010dd6102057fd27a7cc6
Imran Price <imranprice96@gmail.com>
2022-04-29 17:43:06 +1200

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
 const add = function() {
-	
+  /*let result = 0;
+  for(let arg in arguments){
+    result+=parseInt(arguments[arg]);
+  };
+  return result;*/
+  return arguments[0] + arguments[1];
 };
 
 const subtract = function() {
-	
+  return arguments[0] - arguments[1];
 };
 
 const sum = function() {
-	
+	return arguments[0].reduce((a, b) => a + b, 0);
 };
 
 const multiply = function() {
-
+  return arguments[0].reduce((a, b) => a * b, 1);
 };
 
 const power = function() {
-	
+	return arguments[0] ** arguments[1];
 };
 
 const factorial = function() {
-	
+	return arguments[0] === 0 ? 1 : arguments[0] * factorial(arguments[0]-1);
 };
 
 // Do not edit below this line
